46ff77c09f3af957a4fce444d96bf942ddf342e7
Oleg Broytman <phd@phdru.name>
2016-03-30 20:52:11 +0300

    Fix a bug: get translations even if locale isn't set

--- m_librarian/translations.py
+++ m_librarian/translations.py
@@ -4,12 +4,16 @@ import locale
 import os
 
 language = locale.getdefaultlocale()[0]
-mo_filename = os.path.join(
-    os.path.dirname(__file__), 'translations', language + '.mo')
-if os.path.exists(mo_filename):
-    mo_file = open(mo_filename, 'rb')
-    translation = gettext.GNUTranslations(mo_file)
-    mo_file.close()
-else:
-    translation = gettext.NullTranslations()
+translations = None
+
+if language:
+    mo_filename = os.path.join(
+        os.path.dirname(__file__), 'translations', language + '.mo')
+    if os.path.exists(mo_filename):
+        mo_file = open(mo_filename, 'rb')
+        translations = gettext.GNUTranslations(mo_file)
+        mo_file.close()
+
+if translations is None:
+    translations = gettext.NullTranslations()
 translation.install(unicode=True)
