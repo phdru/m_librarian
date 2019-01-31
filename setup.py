7ec8811ea1e63e85f95a766095cb2479a52dc756
Oleg Broytman <phd@phdru.name>
2019-01-31 22:45:19 +0300

    Python 3.7

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     packages=['m_librarian', 'm_librarian.web'],
     package_data={'m_librarian': [
