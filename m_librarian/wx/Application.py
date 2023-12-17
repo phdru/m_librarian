b1dff250905c4fa2350d3e7f97f15c67aceac4bc
Oleg Broytman <phd@phdru.name>
2023-12-17 19:30:06 +0300

    Feat(wx): Create main window
    [skip ci]

--- m_librarian/wx/Application.py
+++ m_librarian/wx/Application.py
@@ -1,4 +1,10 @@
 import wx
 
+
 class Application(wx.App):
-    pass
+
+    def OnInit(self):
+        frame = wx.Frame(None, -1, u"m_Librarian")
+        frame.Show(True)
+        self.SetTopWindow(frame)
+        return True
--- scripts/ml-wx.py
+++ scripts/ml-wx.py
@@ -8,6 +8,7 @@ def main():
     if len(sys.argv) > 1:
         sys.exit("This program doesn't accept any arguments")
     app = Application()
+    app.MainLoop()
 
 
 if __name__ == '__main__':
