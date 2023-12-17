f3b5f514fff84ab8bcf3b2927ce926206294ec4f
Oleg Broytman <phd@phdru.name>
2023-12-17 19:45:46 +0300

    Feat(wx): Create main menu bar, add command "Quit"
    [skip ci]

--- m_librarian/wx/Application.py
+++ m_librarian/wx/Application.py
@@ -1,10 +1,28 @@
+# coding: utf-8
+
 import wx
+
+
+class MainWindow(wx.Frame):
+
+    def InitMenu(self):
+        MenuBar = wx.MenuBar()
+        self.SetMenuBar(MenuBar)
+
+        file_menu = wx.Menu()
+        exit = file_menu.Append(wx.ID_EXIT, u"&Выход", u"Выйти из программы")
+        self.Bind(wx.EVT_MENU, self.OnQuit, exit)
+        MenuBar.Append(file_menu, u"&Файл")
+
+    def OnQuit(self, event):
+        self.Close(True)
 
 
 class Application(wx.App):
 
     def OnInit(self):
-        frame = wx.Frame(None, -1, u"m_Librarian")
+        frame = MainWindow(None, -1, u"m_Librarian")
+        frame.InitMenu()
         frame.Show(True)
         self.SetTopWindow(frame)
         return True
