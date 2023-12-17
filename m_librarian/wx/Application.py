62c3d011985f444f1200aa1fecfc9d0175caea59
Oleg Broytman <phd@phdru.name>
2023-12-17 20:21:04 +0300

    Feat(wx): Add dialig "About"
    [skip ci]

--- m_librarian/wx/Application.py
+++ m_librarian/wx/Application.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
-import wx
+import wx, wx.adv
+from ..__version__ import __version__
 
 
 class MainWindow(wx.Frame):
@@ -14,8 +15,26 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnQuit, exit)
         MenuBar.Append(file_menu, u"&Файл")
 
+        about_menu = wx.Menu()
+        about = about_menu.Append(wx.ID_ABOUT,
+                                  u"&О m_Librarian", u"О m_Librarian")
+        self.Bind(wx.EVT_MENU, self.OnAbout, about)
+        MenuBar.Append(about_menu, u"&О программе")
+
     def OnQuit(self, event):
         self.Close(True)
+
+    def OnAbout(self, event):
+        aboutInfo = wx.adv.AboutDialogInfo()
+        aboutInfo.SetName(u'm_Librarian')
+        aboutInfo.SetVersion(__version__)
+        aboutInfo.SetDescription(
+            u'Библиотекарь для библиотек LibRusEc/Flibusta')
+        aboutInfo.AddDeveloper(u'Олег Бройтман')
+        aboutInfo.SetWebSite(u'https://phdru.name/Software/Python/m_librarian/')
+        aboutInfo.SetCopyright(u'(C) 2023 Олег Бройтман')
+        aboutInfo.SetLicense(u'GPL')
+        wx.adv.AboutBox(aboutInfo)
 
 
 class Application(wx.App):
