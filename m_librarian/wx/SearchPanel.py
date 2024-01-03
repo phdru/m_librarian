5ccddf723234cfc741f767c903f95615e8f41e45
Oleg Broytman <phd@phdru.name>
2024-01-04 01:42:06 +0300

    Feat(wx/search): Add substring search radio buttons
    [skip ci]

--- m_librarian/wx/SearchPanel.py
+++ m_librarian/wx/SearchPanel.py
@@ -13,3 +13,14 @@ class SearchPanel(wx.Panel):
 
         self.search_authors = search_authors = wx.TextCtrl(self)
         search_authors_vsizer.Add(search_authors, 0, wx.EXPAND, 0)
+
+        self.search_substr = search_substr = wx.RadioBox(
+            self,
+            choices=[
+                u'Подстрока в начале',
+                u'Подстрока',
+                u'Точное совпадение',
+            ],
+            majorDimension=1, style=wx.RA_SPECIFY_ROWS
+        )
+        search_authors_vsizer.Add(search_substr)
